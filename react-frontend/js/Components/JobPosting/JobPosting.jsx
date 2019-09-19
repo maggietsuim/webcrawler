aa15e13d1346abb8eebc5b528b66e8f340d87e90
Maggie Tsui <maggie02683@gmail.com>
2019-09-19 19:13:25 +0800

    basic ui for each job post complete

--- react-frontend/js/Components/JobPosting/JobPosting.jsx
+++ react-frontend/js/Components/JobPosting/JobPosting.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 require('./JobPosting.css');
-// import Card from '../Card/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import CheckCircle from '@material-ui/icons/CheckCircle';
+import CheckCircleOutline from '@material-ui/icons/CheckCircleOutline';
+import ShareIcon from '@material-ui/icons/Share';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -17,7 +25,27 @@ function JobPosting(props) {
     <Card className={classes.card}>
       <CardHeader
         title={props.role.CompanyName}
+        subheader={props.role.Role + " " + props.role.Location}
+        avatar={
+          <Avatar aria-label="logo" src={props.role.LogoUrl} />
+        }
       />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          <p dangerouslySetInnerHTML={{__html: props.role.Description}} />
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="check circle">
+          <CheckCircle />
+        </IconButton>
+        <IconButton aria-label="check circle">
+          <CheckCircleOutline />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 }
--- react-frontend/webpack.config.js
+++ react-frontend/webpack.config.js
@@ -29,7 +29,7 @@ const config = {
     },
     plugins: [
         new ExtractTextPlugin('styles.css'),
-    ]
+    ],
 };
 
 module.exports = config;
