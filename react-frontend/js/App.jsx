fdfb46a5aea80f4465844db3a666591770e67544
Maggie Tsui <maggie02683@gmail.com>
2019-10-11 16:02:04 +0800

    added search text field and button UI

--- react-frontend/js/App.jsx
+++ react-frontend/js/App.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react';
 require('./App.css');
 import JobPosting from './Components/JobPosting/JobPosting';
+import TextField from '@material-ui/core/TextField';
+import Search from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+    width: 100,
+  },
+}));
 
 function useFetch(url) {
   const [data, setData] = useState([]);
@@ -16,22 +28,40 @@ function useFetch(url) {
 }
 
 function App() {
+  const classes = useStyles();
+
   const [roles] = useFetch(
       "http://127.0.0.1:5000/db"
   );
   return (
-    <div 
-      style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+    <div
+      className="App"
     >
-      {roles.map((role) => (
-        <div 
-          style={{ margin: 10 }}
+      <div className="App-Search">
+        <TextField
+          id="outlined-with-placeholder"
+          label="Search a Skill"
+          placeholder="e.g. Python"
+          margin="normal"
+          variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button 
+          variant="outlined"
+          color="primary" 
+          className={classes.button} 
+          size="small"
+          onClick={}
         >
-          <JobPosting
-            role = {role}
-          />
-        </div>
-      ))}
+          Search
+        </Button> 
+      </div>      
     </div>
   );
 }
